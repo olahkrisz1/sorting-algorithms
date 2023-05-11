a9894ace8e42a1e625688fec7cfb859587f3bee8
olahkrisz1 <olahkrisz1@gmail.com>
2023-05-11 16:56:29 +0200

    bubblesort added

---
+++ bubblesort.js
@@ -0,0 +1,31 @@
+// PseudoCode:
+
+// 1. declare a function that takes an array as parameter
+// 2. define variable 'swapped' without assisgning any value to it. it will be used to track whether any swaps were made during the iteration.
+// 3. using 'do while' loop to ensure that the algorithm runs at least once and continues as long as swaps are made.
+// 4. set 'swapped' variable to false at the befginning of each iteration. if no swaps occur during iteration, it will remain false, and the loop will terminate.
+// 5. start a 'for' loop to go over elements of array.
+// 6. inside the loop, 'if' statement check if the current element is greater then next element. If it is, swap is needed.
+// 7. performing a swap
+// 8. 'swapped=true', indicating that a swap has been made during the current iteration.
+// 9. '}while(swapped)' closes the loop and checks the value of 'swapped'. if its;s true, the loop continues, indicating that at least one swap was made in the current iteration.
+//10. If it;s false, the loop terminates as the array considered sorted.
+//11. return sorted array
+//12. execute function with array as an argument and console.log
+
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+
+console.log(bubbleSort([4, 8, 5, 2, 9]));
